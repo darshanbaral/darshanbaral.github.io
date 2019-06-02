277c15c54b24abef64faa310c515b3f4bad6f229
Darshan Baral <darshanbaral@gmail.com>
2019-06-01 22:12:14 -0700

    Added kitab

--- homeSrc/main.js
+++ homeSrc/main.js
@@ -1,43 +1,28 @@
-const profile = {
-  name: "Darshan Baral",
-  link: "https://www.darshanbaral.com",
-  projects: {
-    dna: {
-      link: "https://darshanbaral.github.io/dna/",
-      description: "A tool to manipulate dna sequences"
-    },
-    callbreak: {
-      link: "https://darshanbaral.github.io/callbreak/",
-      description: "A tool to record socres during a game of Callbreak"
-    },
-    sada: {
-      link: "https://themes.gohugo.io/sada/",
-      description: "A simple gohugo theme made using Bootstrap 4"
-    },
-    mero: {
-      link: "https://themes.gohugo.io/mero/",
-      description: "gohugo theme for personal blogs"
-    }
-  }
-};
+fetch(
+  "https://raw.githubusercontent.com/darshanbaral/darshanbaral.github.io/master/homeSrc/dbData.json"
+)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(profile) {
+    $(document).ready(function() {
+      let heading = `<h1 class="text-center mt-4" id="heading"><a target="_blank" href="${
+        profile.link
+      }">${profile.name}</a></h1>`;
 
-$(document).ready(function() {
-  let heading = `<h1 class="text-center mt-4" id="heading"><a target="_blank" href="${
-    profile.link
-  }">${profile.name}</a></h1>`;
+      $("#app").append(heading);
 
-  $("#app").append(heading);
+      let myProjects = `<ul id="portFolio"></ul>`;
+      $("#heading").after(myProjects);
 
-  let myProjects = `<ul id="portFolio"></ul>`;
-  $("#heading").after(myProjects);
-
-  for (let key in profile.projects) {
-    let projectItems = `<li>
+      for (let key in profile.projects) {
+        let projectItems = `<li>
         <a href="${profile.projects[key].link}">${key}</a>: ${
-      profile.projects[key].description
-    }
+          profile.projects[key].description
+        }
       </li>`;
-    console.log(myProjects);
-    $("#portFolio").append(projectItems);
-  }
-});
+        console.log(myProjects);
+        $("#portFolio").append(projectItems);
+      }
+    });
+  });
