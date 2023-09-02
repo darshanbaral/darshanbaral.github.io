0a8d4c996791de5383a36f0b0e38cd2fdbbdc60a
Darshan Baral <darshanbaral@gmail.com>
2023-09-02 12:12:33 -0700

    removed console log

--- homeSrc/main.js
+++ homeSrc/main.js
@@ -1,14 +1,12 @@
 fetch(
   "https://raw.githubusercontent.com/darshanbaral/darshanbaral.github.io/master/homeSrc/dbData.json"
 )
-  .then(function(response) {
+  .then(function (response) {
     return response.json();
   })
-  .then(function(profile) {
-    $(document).ready(function() {
-      let heading = `<h1 class="text-center mt-4" id="heading"><a target="_blank" href="${
-        profile.link
-      }">${profile.name}</a></h1>`;
+  .then(function (profile) {
+    $(document).ready(function () {
+      let heading = `<h1 class="text-center mt-4" id="heading"><a target="_blank" href="${profile.link}">${profile.name}</a></h1>`;
 
       $("#app").append(heading);
 
@@ -17,11 +15,9 @@ fetch(
 
       for (let key in profile.projects) {
         let projectItems = `<li>
-        <a href="${profile.projects[key].link}">${key}</a>: ${
-          profile.projects[key].description
-        }
+        <a href="${profile.projects[key].link}">${key}</a>: ${profile.projects[key].description}
       </li>`;
-        console.log(myProjects);
+
         $("#portFolio").append(projectItems);
       }
     });
